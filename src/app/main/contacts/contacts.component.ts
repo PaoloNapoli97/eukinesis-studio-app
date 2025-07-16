1a25d1b029e8f61c91a533ece3684daf1d2016e2
PaoloNapoli97 <paolonapoli@live.it>
2025-07-16 21:29:58 +0200

    Add&Modify ngcontent applist info v0.0.15

--- src/app/main/contacts/contacts.component.ts
+++ src/app/main/contacts/contacts.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
+import { ListComponent } from "../../service/list/list.component";
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contacts',
-  imports: [FormsModule],
+  imports: [FormsModule, ListComponent],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
@@ -19,6 +21,11 @@ export class ContactsComponent {
   // get mailtoLink(): string {
   //   return `mailto:pingopallino9@gmail.com?subject=${this.fullName()}&body=${this.email()}`;
   // }
+
+  contactList = [
+    {description: 'Email',  link: 'mailto:info@eukinesistudio.it', title: 'info@eukinesistudio.it'},
+    { link: '', title: "392 123456789" }
+  ]
   
   onSubmit() {
     let objectTitle: string  = `${this.fullName}: ${this.object}`;
