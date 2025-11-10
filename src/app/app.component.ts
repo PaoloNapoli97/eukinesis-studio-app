1029f78624b623c357e9127b1d6fe1c9afacd9e0
PaoloNapoli97 <paolonapoli@live.it>
2025-11-10 14:57:36 +0100

    Angular SEO + h1 css -  v0.1.04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Meta, Title } from '@angular/platform-browser';
 
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from "./main/main.component";
@@ -11,6 +12,19 @@ import { MainComponent } from "./main/main.component";
   styleUrl: './app.component.css'
 })
 
-export class App {
-  protected title = 'eukinesis-studio-app';
+export class App implements OnInit{
+  protected title = 'eukinesis-studio-app'; // variabile interna per il template
+
+  constructor(private meta: Meta, private titleService: Title) {} // rinominato
+
+  ngOnInit() {
+    // SEO: titolo della pagina
+    this.titleService.setTitle('Eukinesis – Chinesiologia, Posturologia e Pilates a Catania');
+
+    // SEO: meta tag
+    this.meta.addTags([
+      { name: 'description', content: 'Studio Eukinesis a Catania: chinesiologia, posturologia e corsi di Pilates personalizzati.' },
+      { name: 'keywords', content: 'chinesiologia Catania, posturologia Catania, Pilates Catania, riabilitazione postura Catania' }
+    ]);
+  }
 }
