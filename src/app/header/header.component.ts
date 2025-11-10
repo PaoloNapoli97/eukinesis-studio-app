896ed54c7e6899d4447a0cf84c55a125665ffdc8
PaoloNapoli97 <paolonapoli@live.it>
2025-11-10 14:28:46 +0100

    Keyword add v0.1.03

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderLogoComponent } from "./header-logo/header-logo.component";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { HeaderTitleComponent } from './header-title/header-title.component';
 
 @Component({
   selector: 'app-header',
-  imports: [HeaderLogoComponent, NavbarComponent],
+  imports: [HeaderLogoComponent, NavbarComponent, HeaderTitleComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
