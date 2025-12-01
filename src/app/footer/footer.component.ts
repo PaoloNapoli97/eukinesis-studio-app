886f369a51889dd125b7bccf201de577e226f85a
PaoloNapoli97 <paolonapoli@live.it>
2025-12-01 14:07:36 +0100

    Dialog Material v0.1.10

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [MatButtonModule, MatDialogModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
@@ -12,5 +15,23 @@ export class FooterComponent {
     {link: '', text: 'Irene Agata Napoli - P.IVA: IT06158240876'},
     {link: 'mailto:info@eukinesistudio.it', text: 'info@eukinesistudio.it'},
     {link: 'https://maps.google.com/maps?ll=37.515783,15.077035&z=19&t=m&hl=it&gl=US&mapclient=apiv3&cid=8122947406870052066', text: 'Via Cesare Vivante, 67 CT'}
-  ]
+  ];
+
+  readonly dialog = inject(MatDialog);
+
+  openDialog() {
+    const dialogRef = this.dialog.open(FooterPopup);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
+
+@Component({
+  selector: 'dialog-content-example-dialog',
+  templateUrl: 'footer-popup.component.html',
+  imports: [MatDialogModule, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FooterPopup {}
